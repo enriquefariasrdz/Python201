2f6700ef43b3175c811e4a3e0ecd15ed09215d6f
enriquefariasrdz <enriquefariasrdz@gmail.com>
2014-11-28 11:28:44 -0600

    Update print-6.py
    Modification of 2nd print

--- print-6.py
+++ print-6.py
@@ -1,15 +1,9 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 
 #-*-coding: utf-8 -*-
 
- 
-
 print( 'Coordenadas: {latitud}, {longitud}' .format( latitud='37.24N', longitud='-115.81W' ) )
-
- 
 
 coord = { 'latitud': '37.24N', 'longitud': '-115.81W' }
 
- 
-
-print( 'Coordenadas: {latitud}, {longitud}' .format( **coord ) )
+print( "Coordenadas: ", coord )
