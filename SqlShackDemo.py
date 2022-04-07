74b6dc3ac9e97204561f22af6900dc63c940f758
Enrique Farias <enriquefariasrdz@gmail.com>
2022-04-07 14:26:51 -0500

    This file has been obtained from this website to understand how to perform debugging on python scripts specifically on Visual Studio Code
    https://www.sqlshack.com/how-to-debug-python-scripts-in-visual-studio-code/

---
+++ SqlShackDemo.py
@@ -0,0 +1,15 @@
+#Debugging simple variable statements
+from collections import UserList
+
+
+number = 100
+print(f'Number is {number}')
+
+number = 500
+print(f'New number is {number}')
+
+#Debugging a for loop
+userList = ['Rob','Dave', 'Fred']
+
+for user in userList:
+    print(user)
